54f4656214fa115d88280752206f2f83d07494d8
ketkev <kdjoosten@gmail.com>
2021-12-07 21:15:35 +0100

    Prepare day 8

---
+++ Days/Day08.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AoCHelper;
+
+namespace AoC2021.Days
+{
+    public sealed class Day08 : BaseDay
+    {
+        private readonly List<int> _input;
+
+        public Day08()
+        {
+            var input = File.ReadAllText(InputFilePath);
+            _input = input.Split(',').Select(int.Parse).ToList();
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            return new ValueTask<string>("TODO");
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            return new ValueTask<string>("TODO");
+        }
+    }
+}
