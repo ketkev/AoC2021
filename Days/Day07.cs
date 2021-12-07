a1ad7c2aa0d519014892a800ba549fc5c5d98441
ketkev <kdjoosten@gmail.com>
2021-12-07 07:45:44 +0100

    Refactor day 7

--- Days/Day07.cs
+++ Days/Day07.cs
@@ -10,13 +10,12 @@ namespace AoC2021.Days
 {
     public sealed class Day07 : BaseDay
     {
-        private readonly string _input;
-        private List<int> _crabPositions;
+        private readonly List<int> _crabPositions;
 
         public Day07()
         {
-            _input = File.ReadAllText(InputFilePath);
-            _crabPositions = _input.Split(',').Select(int.Parse).ToList();
+            var input = File.ReadAllText(InputFilePath);
+            _crabPositions = input.Split(',').Select(int.Parse).ToList();
         }
 
         public override ValueTask<string> Solve_1()
@@ -24,36 +23,16 @@ namespace AoC2021.Days
             var min = _crabPositions.Min();
             var max = _crabPositions.Max();
 
-            var lowest = int.MaxValue;
-            var position = 0;
+            var result = Enumerable.Range(min, max - min + 1).AsParallel()
+                .Select(i => _crabPositions.Aggregate(0, (res, pos) => res + Math.Abs(i - pos))).Min();
 
-            for (int i = min; i < max; i++)
-            {
-                var fuel = _crabPositions.Aggregate(0, (res, crabPos) => res + Math.Abs(i - crabPos));
-
-                if (fuel < lowest)
-                {
-                    lowest = fuel;
-                    position = i;
-                }
-            }
-
-            return new ValueTask<string>(lowest.ToString());
+            return new ValueTask<string>(result.ToString());
         }
 
-        private int CalculateFuel(int start, int stop)
+        private static int CalculateFuel(int start, int stop)
         {
             var steps = Math.Abs(start - stop);
-
-            var fuel = 0;
-            var step = 1;
-            for (int i = 0; i < steps; i++)
-            {
-                fuel += step;
-                step++;
-            }
-
-            return fuel;
+            return steps * (steps + 1) / 2;
         }
 
         public override ValueTask<string> Solve_2()
@@ -61,22 +40,10 @@ namespace AoC2021.Days
             var min = _crabPositions.Min();
             var max = _crabPositions.Max();
 
-            var lowest = int.MaxValue;
+            var result = Enumerable.Range(min, max - min + 1).AsParallel()
+                .Select(i => _crabPositions.Aggregate(0, (res, pos) => res + CalculateFuel(pos, i))).Min();
 
-            for (int i = min; i < max; i++)
-            {
-                // Replace with n * (n + 1) / 2
-                // ParallelEnumerable.Select for speed
-                var fuel = _crabPositions.Aggregate(0,
-                    (res, crabPos) => res + ((int)Math.Pow(Math.Abs(i - crabPos), 2) + Math.Abs(i - crabPos)) / 2);
-
-                if (fuel < lowest)
-                {
-                    lowest = fuel;
-                }
-            }
-
-            return new ValueTask<string>(lowest.ToString());
+            return new ValueTask<string>(result.ToString());
         }
     }
 }
