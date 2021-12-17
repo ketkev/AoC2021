3b010782f1a00e68c7277b2769edb954e6e2409d
ketkev <kdjoosten@gmail.com>
2021-12-17 22:17:41 +0100

    Prepare day 18

---
+++ Days/Day18.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AoC2021.utils;
+using AoCHelper;
+
+namespace AoC2021.Days
+{
+    public sealed class Day18 : BaseDay
+    {
+        private readonly string _stringInput;
+
+        public Day18()
+        {
+            _stringInput = File.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            return new ValueTask<string>("TODO");
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            return new ValueTask<string>("TODO");
+        }
+    }
+}
